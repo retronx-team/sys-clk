ced5d4ec193e869c0f67979f9a021192ed678cec
p-sam <p-sam@d3vs.net>
2020-04-18 12:25:06 +0000

    Build script dir structure

--- build.sh
+++ build.sh
@@ -19,9 +19,9 @@ pushd "$ROOT_DIR/sysmodule"
 make -j$CORES
 popd > /dev/null
 
-mkdir -p "$DIST_DIR/$TITLE_ID/flags"
-cp -vf "$ROOT_DIR/sysmodule/out/sys-clk.nsp" "$DIST_DIR/$TITLE_ID/exefs.nsp"
->"$DIST_DIR/$TITLE_ID/flags/boot2.flag"
+mkdir -p "$DIST_DIR/atmosphere/contents/$TITLE_ID/flags"
+cp -vf "$ROOT_DIR/sysmodule/out/sys-clk.nsp" "$DIST_DIR/atmosphere/contents/$TITLE_ID/exefs.nsp"
+>"$DIST_DIR/atmosphere/contents/$TITLE_ID/flags/boot2.flag"
 
 echo "*** assets ***"
 mkdir -p "$DIST_DIR/config/sys-clk"
